211736d3a09831b33079568b2eb8ba7d68b9b96a
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-08-12 16:29:23 +0200

    Flag kata works with integer flags

--- kata_flag/flag.go
+++ kata_flag/flag.go
@@ -1,11 +1,22 @@
 package kataflag
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
-// InputType = int, string, stringList
+type FlagValue struct {
+	Bool    bool
+	Integer int
+	String  string
+	Array   []string
+}
+
+// InputType = bool, string, stringList, int
 type Flag struct {
 	Identifier string
 	InputType  int
+	Default    FlagValue
 }
 
 type Schema struct {
@@ -14,10 +25,11 @@ type Schema struct {
 
 func GetSchema() Schema {
 	flags := []Flag{
-		{"d", 0},    // Debug Flag
-		{"v", 0},    // Verbose Flag
-		{"p", 1},    // File path
-		{"args", 2}, // Any amount of string args
+		{"d", 0, FlagValue{}},                   // Debug Flag
+		{"v", 0, FlagValue{}},                   // Verbose Flag
+		{"p", 1, FlagValue{String: "."}},        // File path
+		{"args", 2, FlagValue{}},                // Any amount of string args
+		{"timestamp", 3, FlagValue{Integer: 1}}, // Some timestamp
 	}
 
 	schema := Schema{flags}
@@ -25,15 +37,67 @@ func GetSchema() Schema {
 	return schema
 }
 
-func findFlag(schema Schema, flagIdentifier string) Flag {
+func FindFlag(schema Schema, flagIdentifier string) Flag {
 	for _, flag := range schema.flags {
 		if flag.Identifier == flagIdentifier {
 			return flag
 		}
 	}
-	return Flag{"", 0}
+	return Flag{"", -1, FlagValue{}}
 }
 
+func (f Flag) Evaluate(s string) FlagValue {
+	index := strings.Index(s, "-"+f.Identifier)
+
+	// No flag provided, use default
+	if index == -1 {
+		return f.Default
+	}
+
+	ret := FlagValue{Bool: true}
+
+	content := s[strings.Index(s, "-"+f.Identifier)+len(f.Identifier)+1:]
+	content = strings.TrimSpace(content)
+
+	if f.InputType == 3 {
+		// Integer case
+		minusIndex := strings.Index(content, "-")
+		offset := 0
+		if minusIndex == 0 {
+			// We are an integer and there is a - right next to our flag
+			offset = 1
+		}
+
+		if strings.Contains(content[offset:], "-") {
+			// Cut off all other Flags
+			ret.String = content[0:strings.Index(content[offset:], "-")]
+		}
+		ret.String = strings.TrimSpace(ret.String)
+
+		num, err := strconv.Atoi(ret.String)
+
+		if err != nil {
+			println(err.Error())
+		}
+
+		ret.Integer = num
+
+	} else {
+		if strings.Contains(content, "-") {
+			ret.String = content[0:strings.Index(content, "-")]
+		}
+		ret.String = strings.TrimSpace(ret.String)
+
+		if f.InputType == 2 {
+			// Inteprete content as a list
+			ret.Array = strings.Split(ret.String, ",")
+		}
+	}
+
+	return ret
+}
+
+/*
 func interpreteInput(flag Flag, input string) (bool, string, []string) {
 
 	index := strings.Index(input, "-"+flag.Identifier)
@@ -98,3 +162,4 @@ func GetArgs(schema Schema, input string) []string {
 
 	return output
 }
+*/
--- kata_flag/flag_test.go
+++ kata_flag/flag_test.go
@@ -6,17 +6,17 @@ import (
 )
 
 func TestFlag(t *testing.T) {
-	allFlags := "-d -args what,yes -p ./flag_test.go -v"
+	allFlags := "-d -args what,yes -timestamp 100 -p ./flag_test.go -v"
 
 	t.Run("Test Debug Flag", func(t *testing.T) {
 		want := true
-		got := InDebugMode(GetSchema(), allFlags)
+		got := FindFlag(GetSchema(), "d").Evaluate(allFlags).Bool
 
 		assertCorrectFlag(t, got, want)
 	})
 	t.Run("Test Path", func(t *testing.T) {
 		want := "./flag_test.go"
-		got := GetPath(GetSchema(), allFlags)
+		got := FindFlag(GetSchema(), "p").Evaluate(allFlags).String
 
 		if want != got {
 			t.Errorf("Got %s, want %s", got, want)
@@ -24,7 +24,7 @@ func TestFlag(t *testing.T) {
 	})
 	t.Run("Test Args Inputs", func(t *testing.T) {
 		want := []string{"what", "yes"}
-		got := GetArgs(GetSchema(), allFlags)
+		got := FindFlag(GetSchema(), "args").Evaluate(allFlags).Array
 
 		if !reflect.DeepEqual(want, got) {
 			t.Errorf("Got %s, want %s", got, want)
@@ -32,30 +32,48 @@ func TestFlag(t *testing.T) {
 	})
 	t.Run("Test Verbose Inputs", func(t *testing.T) {
 		want := true
-		got := IsVerbose(GetSchema(), allFlags)
+		got := FindFlag(GetSchema(), "v").Evaluate(allFlags).Bool
 
 		if !reflect.DeepEqual(want, got) {
 			t.Errorf("Got %t, want %t", got, want)
 		}
 	})
+	t.Run("Test Timestamp Inputs", func(t *testing.T) {
+		want := 100
+
+		got := FindFlag(GetSchema(), "timestamp").Evaluate(allFlags).Integer
+
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Got %d, want %d", got, want)
+		}
+	})
+	t.Run("Test Negative Timestamp Inputs", func(t *testing.T) {
+		want := -100
+
+		got := FindFlag(GetSchema(), "timestamp").Evaluate("-d -timestamp -100 -v").Integer
+
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Got %d, want %d", got, want)
+		}
+	})
 	noFlags := ""
 	t.Run("Test No Debug Flag", func(t *testing.T) {
 		want := false
-		got := InDebugMode(GetSchema(), noFlags)
+		got := FindFlag(GetSchema(), "d").Evaluate(noFlags).Bool
 
 		assertCorrectFlag(t, got, want)
 	})
-	t.Run("Test No Path", func(t *testing.T) {
-		want := ""
-		got := GetPath(GetSchema(), noFlags)
+	t.Run("Test Default Path", func(t *testing.T) {
+		want := "."
+		got := FindFlag(GetSchema(), "p").Evaluate(noFlags).String
 
 		if want != got {
 			t.Errorf("Got %s, want %s", got, want)
 		}
 	})
 	t.Run("Test No Args Inputs", func(t *testing.T) {
-		want := []string{""}
-		got := GetArgs(GetSchema(), noFlags)
+		var want []string
+		got := FindFlag(GetSchema(), "args").Evaluate(noFlags).Array
 
 		if !reflect.DeepEqual(want, got) {
 			t.Errorf("Got %s, want %s", got, want)
@@ -64,10 +82,19 @@ func TestFlag(t *testing.T) {
 	t.Run("Test No Verbose Inputs", func(t *testing.T) {
 		want := false
 
-		got := IsVerbose(GetSchema(), noFlags)
+		got := FindFlag(GetSchema(), "v").Evaluate(noFlags).Bool
 
 		if !reflect.DeepEqual(want, got) {
 			t.Errorf("Got %t, want %t", got, want)
+		}
+	})
+	t.Run("Test No Timestamp Inputs", func(t *testing.T) {
+		want := 1
+
+		got := FindFlag(GetSchema(), "timestamp").Evaluate(noFlags).Integer
+
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Got %d, want %d", got, want)
 		}
 	})
 }
