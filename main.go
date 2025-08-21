7cef3999b6d0c81bea7ad2b86139ec9d236f996d
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-08-21 14:22:35 +0200

    HTTP Tutorial

--- main.go
+++ main.go
@@ -4,18 +4,7 @@ import (
 	"log"
 	"net/http"
 )
-
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-func (i *InMemoryPlayerStore) IncPlayerScore(name string) {
-
-}
-
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":5001", server))
 }
